fec50cf2888b3df564b0e2fef9bad929fbb5bcb4
faizan <faizan1000444@email.com>
2025-04-21 13:47:30 +0500

    Splash Screen done with animations

---
+++ app/src/main/java/com/example/gearup/SplashScreen.java
@@ -0,0 +1,53 @@
+package com.example.gearup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+//    hooks
+    ImageView ivLogo;
+    CardView cvLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        init();
+        apply_animation();
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 5000);
+    }
+
+    private void init(){
+        ivLogo=findViewById(R.id.ivLogo);
+        cvLogo=findViewById(R.id.cvLogo);
+    }
+
+    private void apply_animation(){
+        Animation logo_rotating = AnimationUtils.loadAnimation(this, R.anim.logo_rotating);  // Load the rotation animation
+        ivLogo.startAnimation(logo_rotating); //start animation
+    }
+}
