53358453b90941513b4b03365292af80e9209768
faizan <faizan1000444@email.com>
2025-05-07 00:26:38 +0500

    conversion to tabs design done

--- app/src/main/java/com/example/gearup/MainActivity.java
+++ app/src/main/java/com/example/gearup/MainActivity.java
@@ -7,8 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 viewPager;
+    private TabLayout tabLayout;
+    private ViewPagerAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        init();
+
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> {
+                    switch (position) {
+                        case 0:
+                            tab.setText("Home");
+                            tab.setIcon(R.drawable.icon_home); // Add your icon resource here
+                            break;
+                        case 1:
+                            tab.setText("Customers");
+                            tab.setIcon(R.drawable.icon_customers); // Add your icon resource here
+                            break;
+                        case 2:
+                            tab.setText("Services");
+                            tab.setIcon(R.drawable.icon_services); // Add your icon resource here
+                            break;
+                    }
+                }).attach();
+    }
+
+    private void init(){
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+        adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
     }
 }
